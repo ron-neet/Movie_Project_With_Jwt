394e06460e29a6d9f3ce9c551af749872e7044d1
ron-neet <ronitstha17@gmail.com>
2025-06-01 21:10:49 +0545

    Adding serice to add Logic for Operation

---
+++ src/main/java/com/RonitCodes/MovieApi/Service/MovieService.java
@@ -0,0 +1,22 @@
+package com.RonitCodes.MovieApi.Service;
+
+import com.RonitCodes.MovieApi.DTO.MovieDto;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+
+public interface MovieService {
+
+
+    MovieDto addMovie(MovieDto movieDto, MultipartFile file) throws IOException;
+
+    MovieDto getMovie(Integer movieId);
+
+    List<MovieDto> getAllMovies();
+
+    MovieDto updateMovie(Integer movieId ,MovieDto movieDto, MultipartFile file) throws IOException;
+
+    String deleteMovie(Integer movieId);
+}
