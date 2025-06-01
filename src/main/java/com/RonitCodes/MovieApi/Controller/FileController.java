4b1f72d6a8ac3ed16b5aad4f8d384a63b221d881
ron-neet <ronitstha17@gmail.com>
2025-06-01 18:27:45 +0545

    Adding controller to perform operation

---
+++ src/main/java/com/RonitCodes/MovieApi/Controller/FileController.java
@@ -0,0 +1,43 @@
+package com.RonitCodes.MovieApi.Controller;
+
+import com.RonitCodes.MovieApi.Service.FileService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@RestController
+@RequestMapping("/file/")
+public class FileController {
+
+    @Autowired
+    private FileService fileService;
+
+    @Value("${project.poster}")
+    private String path;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFileHandler(@RequestPart MultipartFile file) throws IOException{
+
+        String uploadedFileName = fileService.uploadFile(path, file);
+        return ResponseEntity.ok("File uploaded successfully: " + uploadedFileName);
+    }
+
+    @GetMapping("/{fileName}")
+    public void serveFileHandler(@PathVariable String fileName, HttpServletResponse response ) throws IOException {
+
+        InputStream resourceFile = fileService.getResourceFile(path, fileName);
+
+        response.setContentType(MediaType.IMAGE_PNG_VALUE);
+
+        // To Convert into required response
+        StreamUtils.copy(resourceFile, response.getOutputStream());
+    }
+}
