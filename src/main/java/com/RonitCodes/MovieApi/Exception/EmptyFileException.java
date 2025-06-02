bebc34bfc09c1d6163be8d98cc4849818b6b29eb
ron-neet <ronitstha17@gmail.com>
2025-06-02 11:20:29 +0545

    Handling Exception in organized Format

---
+++ src/main/java/com/RonitCodes/MovieApi/Exception/EmptyFileException.java
@@ -0,0 +1,7 @@
+package com.RonitCodes.MovieApi.Exception;
+
+public class EmptyFileException extends RuntimeException {
+    public EmptyFileException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/RonitCodes/MovieApi/Exception/FileExistsException.java
@@ -0,0 +1,8 @@
+package com.RonitCodes.MovieApi.Exception;
+
+public class FileExistsException extends RuntimeException {
+    public FileExistsException(String message) {
+
+        super(message);
+    }
+}
